cba3fc3fc515c341a0ba60f75c536a654fc13525
FirentisTFW <jkb.pawel@gmail.com>
2020-10-19 19:43:28 +0200

    Added simple test for Validator class.

---
+++ test/validator_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/helpers/validator.dart';
+
+void main() {
+  group('Validator', () {
+    test('validateForEmptyString returns error message when string is empty',
+        () {
+      final result = Validator.validateForEmptyString('');
+
+      expect(result, 'This field cannnot be empty.');
+    });
+
+    test('validateForNumber returns error message when value is not a number',
+        () {
+      final result = Validator.validateForNumber('asd');
+
+      expect(result, 'Enter a number.');
+    });
+  });
+
+  test('validateForNumber returns null when value is a number', () {
+    final result = Validator.validateForNumber('122');
+
+    expect(result, null);
+  });
+
+  test('validateBodyweight returns error message if value is empty', () {
+    final result = Validator.validateBodyweight('');
+
+    expect(result, 'Please provide valid bodyweight in kilograms.');
+  });
+
+  test('validateBodyweight returns error message if value is not a number', () {
+    final result = Validator.validateBodyweight('asd');
+
+    expect(result, 'Please enter a number.');
+  });
+
+  test('validateBodyweight returns error message if value is less than 30', () {
+    final result = Validator.validateBodyweight('29');
+
+    expect(result, 'Please provide valid bodyweight in kilograms.');
+  });
+
+  test('validateBodyweight returns error message if value is more than 300',
+      () {
+    final result = Validator.validateBodyweight('301');
+
+    expect(result, 'Please provide valid bodyweight in kilograms.');
+  });
+
+  test('validateBodyweigth returns null if value is between 30 and 300', () {
+    final result = Validator.validateBodyweight('70');
+
+    expect(result, null);
+  });
+}
